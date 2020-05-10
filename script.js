2746b03f185b8a23d0b950ff67f5c433a8ece402
VaderSteve76 <alverson.s@yahoo.com>
2020-05-09 20:00:04 -0400

    Added filter posts & show loader and fetch more posts

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const postsContainer = document.getElementById('posts-container');
 const loading = document.querySelector('.loader');
 const filter = document.getElementById('filter');
 
-let limit = 3;
+let limit = 5;
 let page = 1;
 
 // Fetch posts from API
@@ -14,7 +14,7 @@ async function getPosts() {
   const data = await res.json();
 
   return data;
-}
+};
 
 // Show posts in DOM
 async function showPosts() {
@@ -33,7 +33,38 @@ async function showPosts() {
 
     postsContainer.appendChild(postEl);
   });
-}
+};
+
+// Show loader & fetch more posts
+function showLoading() {
+  loading.classList.add('show');
+
+  setTimeout(() => {
+    loading.classList.remove('show');
+
+    setTimeout(() => {
+      page++;
+      showPosts();
+    }, 300);
+  }, 700);
+};
+
+// Filter posts by input
+function filterPosts(e) {
+  const term = e.target.value.toUpperCase();
+  const posts = document.querySelectorAll('.post');
+
+  posts.forEach(post => {
+    const title = post.querySelector('.post-title').innerText.toUpperCase();
+    const body = post.querySelector('.post-body').innerText.toUpperCase();
+
+    if (title.indexOf(term) > -1 || body.indexOf(term) > -1) {
+      post.style.display = 'flex';
+    } else {
+      post.style.display = 'none';
+    }
+  });
+};
 
 // Event Listeners
 showPosts();
